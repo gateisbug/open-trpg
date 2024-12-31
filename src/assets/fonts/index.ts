fb5f4d1e2429c7160dd1afef805fdf1ab4014ed6
gateisbug <luftwaffe04@naver.com>
2024-12-31 16:36:54 +0900

    initial commit

--- src/assets/fonts/index.ts
+++
@@ -1,2 +0,0 @@
-// This file exists solely to include the directory in the commit.
-// The index is not particularly useful for optimization, so please delete it when starting development.
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+// noinspection JSUnusedGlobalSymbols
+
+import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+const p = (src: string) => resolve(__dirname, src)
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: p('./src') },
+      { find: '@app', replacement: p('./src/app') },
+      { find: '@assets', replacement: p('./src/assets') },
+      { find: '@components', replacement: p('./src/components') },
+      { find: '@util', replacement: p('./src/util') },
+      { find: '@views', replacement: p('./src/views') },
+      { find: '@parakeet', replacement: p('./xlib/parakeet') },
+    ],
+  },
+  build: {
+    cssTarget: 'chrome58',
+    minify: true,
+    cssMinify: true,
+  },
 })
