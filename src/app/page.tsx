9be41be2c542391782c3a84e01eb575292a6355b
gateisbug <luftwaffe04@naver.com>
2024-12-31 16:46:08 +0900

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function RootPage() {
   return (
-    <main id='page-root'>
+    <main className='layout' id='page-root'>
       <h2>Root Page</h2>
       <p>this is root page</p>
     </main>
